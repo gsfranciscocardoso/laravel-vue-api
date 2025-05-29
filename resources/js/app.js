63b592db354b98c4212c25e3f6d593f2b22ceb0c
franciscocardosodev <francsicocardosodev@gmail.com>
2025-05-29 14:48:12 +0100

    3

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,6 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'https://php-laravel-vue-api-main-5vew6q.laravel.cloud'
 
 createApp(App).use(router).mount('#app')
